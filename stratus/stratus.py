c38587baa927b7007d08487b11d2396e086020c1
John Andersen <johnandersenpdx@gmail.com>
2015-05-04 15:06:12 -0700

    Basics Working

--- stratus/stratus.py
+++ stratus/stratus.py
@@ -15,8 +15,9 @@ import mimetypes
 import datetime
 import time
 import SimpleHTTPSServer
+import copy
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 PORT = 5678
 
 
@@ -24,32 +25,42 @@ class server(SimpleHTTPSServer.handler):
     """docstring for handler"""
     def __init__(self):
         super(server, self).__init__()
-        self.node_timeout(1, 5)
+        self.node_timeout(1, 60)
         self.conns = {}
         self.data = {}
+        self.online = 0
         self.actions = [
             ('post', '/ping/:name', self.post_ping),
             ('get', '/ping/:name', self.get_ping),
+            ('get', '/messages/:name', self.get_messages),
             ('get', '/connected', self.get_connected)
             ]
-    
+
     def log(self, message):
-        print message
+        del message
 
     def date_handler(self, obj):
         return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
-    def get_ping(self, request):
-        self.node_status(request["variables"]["name"], update=True)
-        output = json.dumps(request['variables'])
-        headers = self.create_header()
-        headers["Content-Type"] = "application/json"
-        return self.end_response(headers, output)
-
     def post_ping(self, request):
         self.node_status(request["variables"]["name"], update=True)
-        output = self.form_data(request['data'])
-        output = json.dumps(output)
+        # Add message to be sent out
+        recv_data = self.form_data(request['data'])
+        recv_data = self.message(request["variables"]["name"], \
+            recv_data["data"], recv_data["to"])
+        self.add_message(recv_data)
+        # Get messages for sender
+        return self.get_messages(request)
+
+    def get_ping(self, request):
+        self.node_status(request["variables"]["name"], update=True)
+        # Get messages for sender
+        return self.get_messages(request)
+
+    def get_messages(self, request):
+        # Get messages for sender
+        send_data = self.messages(request["variables"]["name"])
+        output = json.dumps(send_data)
         headers = self.create_header()
         headers["Content-Type"] = "application/json"
         return self.end_response(headers, output)
@@ -59,11 +70,6 @@ class server(SimpleHTTPSServer.handler):
         headers = self.create_header()
         headers["Content-Type"] = "application/json"
         return self.end_response(headers, output)
-
-    def post_response(self, request):
-        headers = self.create_header()
-        headers["Content-Type"] = "application/octet-stream"
-        return self.end_response(headers, request['post']['file_name'])
 
     def start_server(self, address="0.0.0.0", port=PORT, key=False, crt=False):
         thread.start_new_thread(self.update_status, ())
@@ -82,12 +88,17 @@ class server(SimpleHTTPSServer.handler):
         curr_time = datetime.datetime.now()
         if not node_name in self.conns:
             self.conns[node_name] = self.node(node_name, curr_time)
+            self.online += 1
         elif update:
             self.conns[node_name]["last_update"] = curr_time
+            self.conns[node_name]["online"] = True
+            if not self.conns[node_name]["online"]:
+                self.online += 1
         else:
             if curr_time - self.timeout > \
                 self.conns[node_name]["last_update"]:
                 self.conns[node_name]["online"] = False
+                self.online -= 1
 
     def node(self, name, curr_time=False):
         # Don't have to call datetime.datetime.now() is provided
@@ -98,6 +109,34 @@ class server(SimpleHTTPSServer.handler):
             "last_update": curr_time,
             "online": True
         }
+
+    def message(self, sent_by, data, to="all"):
+        return {
+            "to": to,
+            "data": data,
+            "seen": [sent_by]
+        }
+
+    def add_message(self, add):
+        if not add["to"] in self.data:
+            self.data[add["to"]] = []
+        self.data[add["to"]].append(add)
+
+    def messages(self, to):
+        new_messages = []
+        if to in self.data:
+            for item in xrange(0, len(self.data[to])):
+                # Check to see if everyone has seen this message
+                if len(self.data[to][item]["seen"]) >= self.online:
+                    del self.data[to][item]
+                elif not to in self.data[to][item]["seen"]:
+                    # Add to array of seen
+                    self.data[to][item]["seen"].append(to)
+                    # Send the message without the seen array
+                    append = copy.deepcopy(self.data[to][item])
+                    del append["seen"]
+                    new_messages.append(append)
+        return new_messages
 
     def node_timeout(self, loop=False, delta=False):
         if loop:
@@ -134,7 +173,11 @@ class client(object):
         is true.
         """
         try:
-            return json.loads(res)
+            res = json.loads(res)
+            if len(res) > 0 and self.recv:
+                for item in xrange(0, len(res)):
+                    self.recv(res[item])
+            return True
         except (ValueError, KeyError):
             return False
 
@@ -190,7 +233,7 @@ class client(object):
         """
         url = "connected"
         res = self.get(url)
-        return self.return_status(res)
+        return json.loads(res)
 
     def main(self):
         """
@@ -206,7 +249,13 @@ class client(object):
         Queues data for sending
         """
         url = "ping/" + self.name
-        return self.post(url, {"to": to, "data": data})
+        if type(data) != str and type(data) != unicode:
+            data = json.dumps(data)
+        res = self.post(url, {"to": to, "data": data})
+        return self.return_status(res)
+
+def print_recv(data):
+    print data
 
 def main():
     address = "0.0.0.0"
@@ -218,9 +267,14 @@ def main():
     stratus_server = server()
     stratus_server.start_server()
 
-    stratus_client = client()
-    # stratus_client.connect()
-    print stratus_client.send("hello there")
+    stratus_client_two = client(name="two")
+    stratus_client_two.connect()
+    stratus_client_two.recv = print_recv
+
+    stratus_client_one = client(name="one")
+    stratus_client_one.connect()
+    print stratus_client_one.send("hello there", "two")
+
     raw_input("Return Key to exit\n")
 
 
