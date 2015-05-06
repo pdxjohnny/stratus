2ca84a1501888bb7dd709d751db7acda4b70099c
John Andersen <johnandersenpdx@gmail.com>
2015-05-06 16:47:46 -0700

    Minimal client listening working

--- stratus/stratus.py
+++ stratus/stratus.py
@@ -19,6 +19,7 @@ import httplib
 
 VERSION = "0.0.5"
 PORT = 5678
+ALL_CLIENTS = "__all__"
 
 
 class server(SimpleHTTPSServer.handler):
@@ -33,6 +34,7 @@ class server(SimpleHTTPSServer.handler):
         self.actions = [
             ('post', '/ping/:name', self.post_ping),
             ('get', '/ping/:name', self.get_ping),
+            ('get', '/connect/:name', self.get_connect),
             ('get', '/messages/:name', self.get_messages),
             ('get', '/connected', self.get_connected),
             ('get', '/:page', self.get_connected)
@@ -45,17 +47,22 @@ class server(SimpleHTTPSServer.handler):
         return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
     def post_ping(self, request):
-        self.node_status(request["variables"]["name"], update=True, \
-            conn=request["socket"])
+        self.node_status(request["variables"]["name"], update=True)
         # Add message to be sent out
         recv_data = self.form_data(request['data'])
         recv_data = self.message(request["variables"]["name"], \
             recv_data["data"], recv_data["to"])
         self.add_message(recv_data)
+        thread.start_new_thread( self.send_messages, (recv_data["to"], ))
         # Get messages for sender
         return self.get_messages(request)
 
     def get_ping(self, request):
+        self.node_status(request["variables"]["name"], update=True)
+        # Get messages for sender
+        return self.get_messages(request)
+
+    def get_connect(self, request):
         self.node_status(request["variables"]["name"], update=True, \
             conn=request["socket"])
         # Get messages for sender
@@ -116,7 +123,7 @@ class server(SimpleHTTPSServer.handler):
             "online": True
         }
 
-    def message(self, sent_by, data, to="all"):
+    def message(self, sent_by, data, to=ALL_CLIENTS):
         return {
             "to": to,
             "from": sent_by,
@@ -129,22 +136,46 @@ class server(SimpleHTTPSServer.handler):
             self.data[add["to"]] = []
         self.data[add["to"]].append(add)
 
+    def send_to(self, node_name, data):
+        if node_name in self.conns:
+            return self.conns[node_name].sendall(data)
+        return False
+
+    def send_messages(self, to):
+        clients = []
+        if to == ALL_CLIENTS:
+            clients = list(self.conns.keys())
+        else:
+            clients = [to]
+        for client_name in clients:
+            thread.start_new_thread( self.send_message, (client_name, ))
+
+    def send_message(self, to):
+        # Get messages for to
+        send_data = self.messages(to)
+        output = json.dumps(send_data)
+        headers = self.create_header()
+        headers["Content-Type"] = "application/json"
+        self.send_to(to, self.end_response(headers, output) )
+
     def messages(self, to):
         new_messages = []
-        to_and_all = [to, "all"]
+        to_and_all = [to, ALL_CLIENTS]
         for name in to_and_all:
             if name in self.data:
-                for item in xrange(0, len(self.data[name])):
+                for item in self.data[name]:
                     # Check to see if everyone has seen this message
-                    if len(self.data[name][item]["seen"]) >= self.online:
-                        del self.data[name][item]
-                    elif not to in self.data[name][item]["seen"]:
+                    if not to in item["seen"]:
                         # Add to array of seen
-                        self.data[name][item]["seen"].append(to)
+                        item["seen"].append(to)
                         # Send the message without the seen array
-                        append = copy.deepcopy(self.data[name][item])
+                        append = copy.deepcopy(item)
                         del append["seen"]
                         new_messages.append(append)
+                for item in xrange(0, len(self.data[name])):
+                    if len(self.data[name]) and \
+                        len(self.data[name][item]["seen"]) >= self.online:
+                        del self.data[name][item]
         return new_messages
 
     def node_timeout(self, loop=False, delta=False):
@@ -159,7 +190,7 @@ class server(SimpleHTTPSServer.handler):
 class client(object):
     """docstring for client"""
     def __init__(self, host="localhost", port=PORT, protocol="http", \
-        name=socket.gethostname(), update=1, recv=False):
+        name=socket.gethostname(), update=20, recv=False):
         super(client, self).__init__()
         self.host = host
         self.port = port
@@ -178,7 +209,7 @@ class client(object):
         self.headers = {"Connection": "keep-alive"}
         self.ping_conn = httplib.HTTPConnection(host)
         self.send_conn = httplib.HTTPConnection(host)
-        # self.recv_conn = httplib.HTTPConnection(host)
+        self.recv_conn = httplib.HTTPConnection(host)
         return True
 
     def return_status(self, res):
@@ -205,37 +236,14 @@ class client(object):
         except (ValueError, KeyError):
             return False
 
-    def _get(self, url, http_conn):
-        """
-        Requests the page and returns data
-        """
-        res = ""
-        # try:
-        http_conn.request("GET", url, headers=self.headers)
-        res = http_conn.getresponse()
-        res = res.read()
-        # except (httplib.URLError, httplib.HTTPError), error:
-        #     print error
-        return res
-
     def get(self, url, http_conn):
         """
         Requests the page and returns data
         """
-        url = "/" + url
-        return self._get(url, http_conn)
-
-    def _post(self, url, data):
-        """
-        Requests the page and returns data
-        """
         res = ""
         # try:
-        headers = self.headers.copy()
-        headers["Content-Type"] = "application/x-www-form-urlencoded"
-        data = urllib.urlencode(data, True).replace("+", "%20")
-        self.send_conn.request("POST", url, data, headers)
-        res = self.send_conn.getresponse()
+        http_conn.request("GET", "/" + url, headers=self.headers)
+        res = http_conn.getresponse()
         res = res.read()
         # except (httplib.URLError, httplib.HTTPError), error:
         #     print error
@@ -245,8 +253,17 @@ class client(object):
         """
         Requests the page and returns data
         """
-        url = "/" + url
-        return self._post(url, data)
+        res = ""
+        # try:
+        headers = self.headers.copy()
+        headers["Content-Type"] = "application/x-www-form-urlencoded"
+        data = urllib.urlencode(data, True).replace("+", "%20")
+        self.send_conn.request("POST", "/" + url, data, headers)
+        res = self.send_conn.getresponse()
+        res = res.read()
+        # except (httplib.URLError, httplib.HTTPError), error:
+        #     print error
+        return res
 
     def connect(self):
         """
@@ -258,10 +275,31 @@ class client(object):
         """
         Continues to ping
         """
+        self.recv_connect()
         while True:
             self.ping()
             time.sleep(self.update)
         return 0
+
+    def recv_connect(self):
+        """
+        Connects a socket that the server can push to.
+        """
+        url = "/connect/" + self.name
+        self.recv_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.recv_conn.connect((self.host, self.port))
+        self.recv_conn.sendall('GET %s HTTP/1.1\r\nConnection: keep-alive' % (url, ))
+        data = self.recv_conn.recv(1024)
+        res = "\n\n".join(data.split("\n\n")[1:])
+        self.return_status(res)
+        self.listen()
+
+    def listen(self):
+        while True:
+            data = self.recv_conn.recv(1024)
+            res = "\n\n".join(data.split("\n\n")[1:])
+            if len(res):
+                self.return_status(res)
 
     def ping(self):
         """
@@ -271,7 +309,7 @@ class client(object):
         res = self.get(url, self.ping_conn)
         return self.return_status(res)
 
-    def send(self, data, to="all"):
+    def send(self, data, to=ALL_CLIENTS):
         """
         Queues data for sending
         """
@@ -303,7 +341,7 @@ class client(object):
 
 
 def print_recv(data):
-    print data["__name__"], data
+    print '\n' + data["__name__"], data
 
 def main():
     address = "0.0.0.0"
@@ -325,7 +363,7 @@ def main():
     while True:
         data = raw_input("Send data: ")
         if len(data) > 0:
-            stratus_client_one.send(data)
+            stratus_client_one.send(data, "two")
         pass
 
 
