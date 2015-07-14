8806e89946c020b4c70c198c2d75042f9f90e550
John Andersen <johnandersenpdx@gmail.com>
2015-07-14 15:14:46 -0700

    Addition of services, calling functions on services, and fault tollerent servers / nodes (stratus class)

--- stratus/stratus.py
+++ stratus/stratus.py
@@ -23,7 +23,7 @@ import SimpleHTTPSServer
 
 import sockhttp
 
-__version__ = "0.0.27"
+__version__ = "0.0.28"
 __description__ = "Connection facilitator"
 __logo__ = """
  ___  ____  ____    __   ____  __  __  ___
